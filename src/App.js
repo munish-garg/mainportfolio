c0a81c822e0b556ddfbf5a105e0f6a00184ff6ac
munish-garg <munishkumar123264@gmail.com>
2021-01-20 19:50:55 +0530

    2ndCommit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+
+//NavBar
 import Navbar from './Components/Navbar/Navbar';
+
+//Header
+import Header from './Components/Header/Header';
+
+//Particles
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Particles
+      params={{
+        particles:{
+          number:{
+            value:40,
+            density:{
+              enable:true,
+              value_area:1000
+            }
+          },
+          shape:{
+            type:"circle",
+            stroke:{
+              width: 6,
+              color: "#f9ab00"
+            }
+          }
+        }
+      }}
+      />
+      <Navbar/>
+      <Header/>
+    </>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './header.css';
+import Typical from 'react-typical';
+
+const Header = () =>{
+    return (
+        <>
+            <header id="header-part">
+                <div className="outer-div">
+                    <div className="inner-div">
+                        <h1>Myself Munish Kumar Garg</h1>
+                    </div>
+                    <Typical
+                    steps={['Student', 1000, 'Developer', 1000, 'Programmer', 1000]}
+                    loop={Infinity}
+                    wrapper="div"/>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Header;
