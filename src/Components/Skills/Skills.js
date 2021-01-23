a47b3816b6289e8d489ad8dd4b4f8d400f77930a
munishgarg-02 <munishkr.garg@yahoo.in>
2021-01-24 01:31:08 +0530

    addedImagesInANewManner

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,37 +1,89 @@
 import React from 'react';
 import './skills.css';
+import html5 from '../Skills/images/html5.png';
+import css3 from '../Skills/images/css3.png';
+import js from '../Skills/images/js.png';
+import cpp from '../Skills/images/cpp.png';
+import python2 from '../Skills/images/python2.png';
+import react4 from '../Skills/images/react4.png';
+import redux from '../Skills/images/redux.png';
+import django from '../Skills/images/django.png';
+
+
 
 const Skills = () =>{
     return (
         <>
             <div className="outer-container">
                 <div className="container">
+                    <h2>SKILLS KNOWN TO ME ARE:</h2>
                     <div className="row">
                         <div className="col-lg-3">
-                            <div className="card border-0" id="card-1"></div>
+                            <div className="card">
+                                <div className="card-img">
+                                    <img src={html5} alt="html5"/>
+                                </div>
+                                <div className="card-content"></div>
+                            </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card border-0" id="card-2"></div>
+                            <div className="card">
+                                <div className="card-img">
+                                    <img src={css3} alt="css3"/>
+                                </div>
+                                <div className="card-content"></div>
+                            </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card border-0" id="card-3"></div>
+                            <div className="card">
+                                <div className="card-img">
+                                    <img src={js} alt="JavaScipt"/>
+                                </div>
+                                <div className="card-content"></div>
+                            </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card border-0" id="card-4"></div>
+                            <div className="card">
+                                <div className="card-img">
+                                    <img src={cpp} alt="C++"/>
+                                </div>
+                                <div className="card-content"></div>
+                            </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-lg-3">
-                            <div className="card border-0" id="card-5"></div>
+                            <div className="card">
+                                <div className="card-img">
+                                    <img src={python2} alt="Python"/>
+                                </div>
+                                <div className="card-content"></div>
+                            </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card border-0" id="card-6"></div>
+                            <div className="card">
+                                <div className="card-img">
+                                    <img src={react4} alt="React"/>
+                                </div>
+                                <div className="card-content"></div>
+                            </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card border-0" id="card-7"></div>
+                            <div className="card">
+                                <div className="card-img">
+                                    <img src={redux} alt="Redux"/>
+                                </div>
+                                <div className="card-content">
+                                </div>
+                            </div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card border-0" id="card-8"></div>
+                            <div className="card">
+                                <div className="card-img">
+                                    <img src={django} alt="Django"/>
+                                </div>
+                                <div className="card-content"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
