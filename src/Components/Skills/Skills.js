596840c5bb6381e4784483c65aa0d262fe779864
munishgarg-02 <munishkr.garg@yahoo.in>
2021-01-23 17:28:05 +0530

    FreshCommit

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,60 +1,37 @@
 import React from 'react';
-import './skills.css'
+import './skills.css';
 
 const Skills = () =>{
     return (
         <>
             <div className="outer-container">
-                <h2>The Skills known to me are:</h2>
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-3">
-                            <div className="card">
-                                <div className="card-image1"></div>
-                                <div className="card-content"></div>
-                            </div>
+                            <div className="card border-0" id="card-1"></div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card">
-                                <div className="card-image2"></div>
-                                <div className="card-content"></div>
-                            </div>
+                            <div className="card border-0" id="card-2"></div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card">
-                                <div className="card-image3"></div>
-                                <div className="card-content"></div>
-                            </div>
+                            <div className="card border-0" id="card-3"></div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card">
-                                <div className="card-image4"></div>
-                                <div className="card-content"></div>
-                            </div>
+                            <div className="card border-0" id="card-4"></div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-3">
+                            <div className="card border-0" id="card-5"></div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card1">
-                                <div className="card-image5"></div>
-                                <div className="card-content1"></div>
-                            </div>
+                            <div className="card border-0" id="card-6"></div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card1">
-                                <div className="card-image6"></div>
-                                <div className="card-content1"></div>
-                            </div>
+                            <div className="card border-0" id="card-7"></div>
                         </div>
                         <div className="col-lg-3">
-                            <div className="card1">
-                                <div className="card-image7"></div>
-                                <div className="card-content1"></div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3">
-                            <div className="card1">
-                                <div className="card-image8"></div>
-                                <div className="card-content1"></div>
-                            </div>
+                            <div className="card border-0" id="card-8"></div>
                         </div>
                     </div>
                 </div>
