788e40fa1b0e99350cd28368483cc2a03ae7210a
munishgarg-02 <munishkr.garg@yahoo.in>
2021-01-26 00:34:13 +0530

    vhIncreased

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -12,13 +12,13 @@ const Education = () =>{
                     <VerticalTimeline>
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
-                            contentStyle={{ background: 'transparent', color: '#fff' }}
+                            contentStyle={{ background: 'transparent', color: 'yellow' }}
                             contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                             date="2015"
                             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                             // icon={<WorkIcon />}
                         >
-                            <h3 className="vertical-timeline-element-title">PASSED CLASS Xth</h3>
+                            <h4 className="vertical-timeline-element-title">PASSED CLASS Xth</h4>
                             <h4 className="vertical-timeline-element-subtitle">CGPA: 10</h4>
                             <p>
                             SCHOOL: VIDYA MANDIR PUBLIC SCHOOL, SECTOR 15A, FARIDABAD, HARYANA
@@ -26,13 +26,13 @@ const Education = () =>{
                         </VerticalTimelineElement>
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
-                            contentStyle={{background:'transparent', color: '#fff'}}
+                            contentStyle={{background:'transparent', color: 'yellow'}}
                             contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                             date="2017"
                             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                             // icon={<WorkIcon />}
                         >
-                            <h3 className="vertical-timeline-element-title">PASSED CLASS XIIth</h3>
+                            <h4 className="vertical-timeline-element-title">PASSED CLASS XIIth</h4>
                             <h4 className="vertical-timeline-element-subtitle">PERCENTAGE: 92.2%</h4>
                             <p>
                             SCHOOL: VIDYA MANDIR PUBLIC SCHOOL, SECTOR 15A, FARIDABAD, HARYANA
@@ -41,10 +41,12 @@ const Education = () =>{
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
                             date="2018-PRESENT"
+                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                            contentStyle={{ background: 'transparent', color: 'yellow' }}
                             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                             // icon={<WorkIcon />}
                         >
-                            <h3 className="vertical-timeline-element-title">PURSUING B.TECH IN COMP. ENGINEERING</h3>
+                            <h4 className="vertical-timeline-element-title">PURSUING B.TECH IN COMP. ENGINEERING</h4>
                             <h4 className="vertical-timeline-element-subtitle">CGPA: 8.61(TILL 4TH SEM)</h4>
                             <p>
                             UNIVERSITY: J.C. BOSE UNIVERSITY OF SCIENCE AND TECHNOLOGY, FARIDABAD, HARYANA
